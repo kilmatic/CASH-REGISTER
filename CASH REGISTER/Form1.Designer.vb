3475e4ca7c3c6e453ed4a3314849737ee9954870
Kanya <kilakanya@gmail.com>
2022-02-10 15:33:51 +0200

    Paths

--- CASH REGISTER/Form1.Designer.vb
+++ CASH REGISTER/Form1.Designer.vb
@@ -22,54 +22,57 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.ListBox1 = New System.Windows.Forms.ListBox()
-        Me.ListBox2 = New System.Windows.Forms.ListBox()
-        Me.ListBox3 = New System.Windows.Forms.ListBox()
+        Me.lstReg = New System.Windows.Forms.ListBox()
+        Me.lstKid = New System.Windows.Forms.ListBox()
+        Me.lstOrd = New System.Windows.Forms.ListBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button3 = New System.Windows.Forms.Button()
-        Me.Button4 = New System.Windows.Forms.Button()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.btnReg = New System.Windows.Forms.Button()
+        Me.btnKid = New System.Windows.Forms.Button()
+        Me.btnClr = New System.Windows.Forms.Button()
+        Me.btnQiut = New System.Windows.Forms.Button()
+        Me.txtTotal = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
-        'ListBox1
+        'lstReg
         '
-        Me.ListBox1.FormattingEnabled = True
-        Me.ListBox1.ItemHeight = 15
-        Me.ListBox1.Location = New System.Drawing.Point(12, 100)
-        Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(177, 94)
-        Me.ListBox1.TabIndex = 0
+        Me.lstReg.FormattingEnabled = True
+        Me.lstReg.ItemHeight = 20
+        Me.lstReg.Location = New System.Drawing.Point(14, 133)
+        Me.lstReg.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.lstReg.Name = "lstReg"
+        Me.lstReg.Size = New System.Drawing.Size(202, 124)
+        Me.lstReg.TabIndex = 0
         '
-        'ListBox2
+        'lstKid
         '
-        Me.ListBox2.FormattingEnabled = True
-        Me.ListBox2.ItemHeight = 15
-        Me.ListBox2.Location = New System.Drawing.Point(610, 100)
-        Me.ListBox2.Name = "ListBox2"
-        Me.ListBox2.Size = New System.Drawing.Size(177, 94)
-        Me.ListBox2.TabIndex = 1
+        Me.lstKid.FormattingEnabled = True
+        Me.lstKid.ItemHeight = 20
+        Me.lstKid.Location = New System.Drawing.Point(697, 133)
+        Me.lstKid.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.lstKid.Name = "lstKid"
+        Me.lstKid.Size = New System.Drawing.Size(202, 124)
+        Me.lstKid.TabIndex = 1
         '
-        'ListBox3
+        'lstOrd
         '
-        Me.ListBox3.FormattingEnabled = True
-        Me.ListBox3.ItemHeight = 15
-        Me.ListBox3.Location = New System.Drawing.Point(302, 100)
-        Me.ListBox3.Name = "ListBox3"
-        Me.ListBox3.Size = New System.Drawing.Size(177, 169)
-        Me.ListBox3.TabIndex = 2
+        Me.lstOrd.FormattingEnabled = True
+        Me.lstOrd.ItemHeight = 20
+        Me.lstOrd.Location = New System.Drawing.Point(345, 133)
+        Me.lstOrd.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.lstOrd.Name = "lstOrd"
+        Me.lstOrd.Size = New System.Drawing.Size(202, 224)
+        Me.lstOrd.TabIndex = 2
         '
         'Label1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.Label1.Location = New System.Drawing.Point(74, 49)
+        Me.Label1.Location = New System.Drawing.Point(63, 65)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(85, 15)
+        Me.Label1.Size = New System.Drawing.Size(107, 20)
         Me.Label1.TabIndex = 3
         Me.Label1.Text = "Regular Menu"
         '
@@ -77,9 +80,9 @@ Partial Class Form1
         '
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.Label2.Location = New System.Drawing.Point(346, 49)
+        Me.Label2.Location = New System.Drawing.Point(386, 65)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(97, 15)
+        Me.Label2.Size = New System.Drawing.Size(121, 20)
         Me.Label2.TabIndex = 4
         Me.Label2.Text = "Customer Order"
         '
@@ -87,81 +90,87 @@ Partial Class Form1
         '
         Me.Label3.AutoSize = True
         Me.Label3.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.Label3.Location = New System.Drawing.Point(669, 49)
+        Me.Label3.Location = New System.Drawing.Point(761, 65)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(65, 15)
+        Me.Label3.Size = New System.Drawing.Size(83, 20)
         Me.Label3.TabIndex = 5
         Me.Label3.Text = "Kids Menu"
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(570, 240)
+        Me.Label4.Location = New System.Drawing.Point(651, 320)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(41, 15)
+        Me.Label4.Size = New System.Drawing.Size(45, 20)
         Me.Label4.TabIndex = 6
-        Me.Label4.Text = "Label4"
+        Me.Label4.Text = "Total:"
         '
-        'Button1
+        'btnReg
         '
-        Me.Button1.Location = New System.Drawing.Point(222, 136)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(50, 23)
-        Me.Button1.TabIndex = 7
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btnReg.Location = New System.Drawing.Point(254, 181)
+        Me.btnReg.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnReg.Name = "btnReg"
+        Me.btnReg.Size = New System.Drawing.Size(57, 31)
+        Me.btnReg.TabIndex = 7
+        Me.btnReg.Text = ">>"
+        Me.btnReg.UseVisualStyleBackColor = True
         '
-        'Button2
+        'btnKid
         '
-        Me.Button2.Location = New System.Drawing.Point(521, 136)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(50, 23)
-        Me.Button2.TabIndex = 8
-        Me.Button2.Text = "Button2"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.btnKid.Location = New System.Drawing.Point(595, 181)
+        Me.btnKid.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnKid.Name = "btnKid"
+        Me.btnKid.Size = New System.Drawing.Size(57, 31)
+        Me.btnKid.TabIndex = 8
+        Me.btnKid.Text = "<<"
+        Me.btnKid.UseVisualStyleBackColor = True
         '
-        'Button3
+        'btnClr
         '
-        Me.Button3.Location = New System.Drawing.Point(12, 232)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(75, 23)
-        Me.Button3.TabIndex = 9
-        Me.Button3.Text = "Button3"
-        Me.Button3.UseVisualStyleBackColor = True
+        Me.btnClr.Location = New System.Drawing.Point(14, 309)
+        Me.btnClr.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnClr.Name = "btnClr"
+        Me.btnClr.Size = New System.Drawing.Size(86, 31)
+        Me.btnClr.TabIndex = 9
+        Me.btnClr.Text = "Clear"
+        Me.btnClr.UseVisualStyleBackColor = True
         '
-        'Button4
+        'btnQiut
         '
-        Me.Button4.Location = New System.Drawing.Point(114, 232)
-        Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(75, 23)
-        Me.Button4.TabIndex = 10
-        Me.Button4.Text = "Button4"
-        Me.Button4.UseVisualStyleBackColor = True
+        Me.btnQiut.Location = New System.Drawing.Point(130, 309)
+        Me.btnQiut.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnQiut.Name = "btnQiut"
+        Me.btnQiut.Size = New System.Drawing.Size(86, 31)
+        Me.btnQiut.TabIndex = 10
+        Me.btnQiut.Text = "Quit"
+        Me.btnQiut.UseVisualStyleBackColor = True
         '
-        'TextBox1
+        'txtTotal
         '
-        Me.TextBox1.Location = New System.Drawing.Point(646, 237)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(141, 23)
-        Me.TextBox1.TabIndex = 11
+        Me.txtTotal.Location = New System.Drawing.Point(738, 316)
+        Me.txtTotal.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txtTotal.Name = "txtTotal"
+        Me.txtTotal.Size = New System.Drawing.Size(161, 27)
+        Me.txtTotal.TabIndex = 11
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(799, 319)
-        Me.Controls.Add(Me.TextBox1)
-        Me.Controls.Add(Me.Button4)
-        Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
+        Me.ClientSize = New System.Drawing.Size(913, 425)
+        Me.Controls.Add(Me.txtTotal)
+        Me.Controls.Add(Me.btnQiut)
+        Me.Controls.Add(Me.btnClr)
+        Me.Controls.Add(Me.btnKid)
+        Me.Controls.Add(Me.btnReg)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.ListBox3)
-        Me.Controls.Add(Me.ListBox2)
-        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.lstOrd)
+        Me.Controls.Add(Me.lstKid)
+        Me.Controls.Add(Me.lstReg)
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
@@ -169,16 +178,16 @@ Partial Class Form1
 
     End Sub
 
-    Friend WithEvents ListBox1 As ListBox
-    Friend WithEvents ListBox2 As ListBox
-    Friend WithEvents ListBox3 As ListBox
+    Friend WithEvents lstReg As ListBox
+    Friend WithEvents lstKid As ListBox
+    Friend WithEvents lstOrd As ListBox
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
     Friend WithEvents Label4 As Label
-    Friend WithEvents Button1 As Button
-    Friend WithEvents Button2 As Button
-    Friend WithEvents Button3 As Button
-    Friend WithEvents Button4 As Button
-    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents btnReg As Button
+    Friend WithEvents btnKid As Button
+    Friend WithEvents btnClr As Button
+    Friend WithEvents btnQiut As Button
+    Friend WithEvents txtTotal As TextBox
 End Class
